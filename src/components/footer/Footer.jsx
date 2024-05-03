bfbb997c6d562559ea228141014803b441ba0775
sissisix <37244114+sissisix@users.noreply.github.com>
2024-05-03 16:40:55 +0200

    Titre accueil block 2 plus petit en mobile + réparation du footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
-import './footer.css';
+import './Footer.css';
 import SocialButtons from "../socialButton/SocialButtons";
 
 
@@ -11,17 +11,18 @@ const Footer = () => {
     return (
         <div className="width-full bg-lightGray border-t border-solid">
             <div className="mx-auto max-w-7xl px-8 md:px-28 lg:px-44">
-                <div className="relative flex h-16 items-center justify-center text-lightBlack">
-                    <div className="absolute inset-y-0 left-0 flex items-center">
+                <div className=" space-x-1 relative flex h-16 items-center justify-center text-lightBlack">
+                    <div className="relative flex h-fit left-0 sm:w-1/2 sm:mb-0  items-start justify-start">
                         <NavLink
                             key="Accueil"
                             to="/"
                         >
-                            <h1 className="text-xl font-bold h-auto w-auto font-title">{title}</h1>
+                            <h1 className="hidden sm:block text-xl font-bold h-auto w-auto font-title">{title}</h1>
+                            <img className="sm:hidden w-8" src="/icn_logo.png"/>
                         </NavLink>
 
                     </div>
-                    <div className="absolute flex inset-y-0 right-0 items-center">
+                    <div className="relative flex flex-col right-0 md:w-1/2 justify-end items-end">
                         <SocialButtons/>
                     </div>
                 </div>
