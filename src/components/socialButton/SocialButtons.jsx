7fd591d98dcc1460ff8a22e396cdb02c479f7280
sissisix <37244114+sissisix@users.noreply.github.com>
2024-06-07 16:04:16 +0200

    Ajout lien linkedin et instagram

--- src/components/socialButton/SocialButtons.jsx
+++ src/components/socialButton/SocialButtons.jsx
@@ -6,9 +6,9 @@ import icnTwit from "../../assets/icones/icn_reseaux/Icn_Twitter.png";
 const SocialButtons = () => {
     return (
         <div className="flex">
-            <a className="icnSocial disable" href="https://fr.linkedin.com/in/simon-suzac-939647154?trk=public-profile-badge-profile-badge-view-profile-cta" target="_blank" rel="noopener noreferrer"><img src={icnlink} alt="Avatar" /></a>
+            <a className="icnSocial" href="https://fr.linkedin.com/in/simon-suzac-939647154?trk=public-profile-badge-profile-badge-view-profile-cta" target="_blank" rel="noopener noreferrer"><img src={icnlink} alt="Avatar" /></a>
             <a className="icnSocial disable" href="#" target="_blank" rel="noopener noreferrer"><img src={icnFcb} alt="Avatar" /></a>
-            <a className="icnSocial disable" href="#" target="_blank" rel="noopener noreferrer"><img src={icnInst} alt="Avatar" /></a>
+            <a className="icnSocial" href="https://www.instagram.com/au_pro_gramme/" target="_blank" rel="noopener noreferrer"><img src={icnInst} alt="Avatar" /></a>
             <a className="icnSocial disable" href="#" target="_blank" rel="noopener noreferrer"><img src={icnTwit} alt="Avatar" /></a>
         </div>
     )
